2779a1c40cb5024362d02cb700a4d16d342650bd
Gilbert <gilbert.tekeoh@outlook.com>
2018-01-22 21:29:39 +0100

    renamed variables

--- src/me/walls/Main.java
+++ src/me/walls/Main.java
@@ -3,45 +3,48 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        double nD,dA,fA,nW, wA, nF,nB,bA,sA,nS, cpsm;
+        double n_Doors,door_A,front_A,n_Windows, window_A, n_Fronts,
+                n_Backs,back_A,side_A,n_Side, cpsm;
 
         Scanner in = new Scanner(System.in);
 
         System.out.println ("Enter #Doors");
-        nD = in.nextInt();
+        n_Doors = in.nextInt();
 
         System.out.println ("Enter DoorArea");
-        dA = in.nextInt();
+        door_A = in.nextInt();
 
         System.out.println ("Enter #windows");
-        nW = in.nextDouble();
+        n_Windows = in.nextDouble();
 
         System.out.println ("Enter WindowArea");
-        wA = in.nextInt();
+        window_A = in.nextInt();
 
         System.out.println ("Enter FrontArea");
-        fA = in.nextDouble();
+        front_A = in.nextDouble();
 
         System.out.println ("Enter #Front");
-        nF = in.nextDouble();
+        n_Fronts = in.nextDouble();
 
         System.out.println ("Enter BackArea");
-        bA = in.nextDouble();
+        back_A = in.nextDouble();
 
         System.out.println ("Enter #backNumbers");
-        nB = in.nextDouble();
+        n_Backs = in.nextDouble();
 
         System.out.println ("Enter SideArea");
-        sA = in.nextDouble();
+        side_A = in.nextDouble();
 
         System.out.println ("Enter #sides");
-        nS = in.nextDouble();
+        n_Side = in.nextDouble();
 
         System.out.println ("Enter cpsm");
         cpsm = in.nextInt();
 
 
-        System.out.println("The cost of the paintings will be $ "+(((((nF*fA)+(nB*bA)+(nS*sA))-(nD*dA)+(nW*wA)))*cpsm));
+        System.out.println("The cost of the paintings will be $ "+
+                (((((n_Fronts*front_A)+(n_Backs*back_A)+(n_Side*side_A))
+                        -(n_Doors*door_A)+(n_Windows*window_A)))*cpsm));
 
 
     }
